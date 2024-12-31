74c1a63e3a1112c9419b4fad6f69e89e626f99dc
GuhttaninSite <caracakk95@gmail.com>
2024-12-31 12:38:14 -0300

    index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+alert("Site e bot desenvolvidos por Guhttanin.")
+
 function copiarTexto() {
     const copiar = "script"
-
+    
     navigator.clipboard.writeText(copiar)
 }
 
 const sim = document.getElementById("sim")
-    const botao1 = document.getElementById("botao")
+const botao1 = document.getElementById("botao")
     
+setTimeout(function(){
     botao1.addEventListener("click", function() {
         sim.style.opacity = 1;
     });
+},1000)
